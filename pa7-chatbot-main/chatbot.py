305fd82b4c261b69c86d1aad6dfa6e80aadfbafc
RuishiCh-git <ruishichen0314@gmail.com>
2024-03-10 13:37:11 -0700

    ChatBot Name

--- pa7-chatbot-main/chatbot.py
+++ pa7-chatbot-main/chatbot.py
@@ -17,7 +17,7 @@ class Chatbot:
     def __init__(self, llm_enabled=False):
         # The chatbot's default name is `moviebot`.
         # TODO: Give your chatbot a new name.
-        self.name = 'moviebot'
+        self.name = 'EduMot'
 
         self.llm_enabled = llm_enabled
 
