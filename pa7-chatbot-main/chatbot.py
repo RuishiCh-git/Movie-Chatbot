99298d0769c2bd6ce1d027dd4b9c4d473c883a5c
RuishiCh-git <ruishichen0314@gmail.com>
2024-03-10 16:10:10 -0700

    self.ratings update

--- pa7-chatbot-main/chatbot.py
+++ pa7-chatbot-main/chatbot.py
@@ -24,7 +24,7 @@ class Chatbot:
         # This matrix has the following shape: num_movies x num_users
         # The values stored in each row i and column j is the rating for
         # movie i by user j
-        self.titles, self.ratings = util.load_ratings('data/ratings.txt')
+        self.titles, ratings = util.load_ratings('data/ratings.txt')
         self.sentiment = util.load_sentiment_dictionary('data/sentiment.txt')
         self.movies = util.load_titles('data/movies.txt')
         ########################################################################
